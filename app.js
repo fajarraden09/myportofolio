92a483f4f99a4e700ce5d2d715612b79dbff59d6
fajarraden09 <fajarraden9019@gmail.com>
2025-09-27 19:32:36 +0700

    edit ke 14

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const hamburger = document.querySelector('#header .hamburger');
-const mobile_menu = document.querySelector('#header .nav-list ul');
+// AMBIL KODE INI DAN GANTI SELURUH ISI FILE app.js ANDA
 
+const hamburger = document.querySelector('#header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('#header .nav-bar .nav-list ul');
+const menu_items = document.querySelectorAll('#header .nav-bar .nav-list ul li a');
+
+// Fungsi untuk membuka & menutup menu hamburger
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
+});
+
+// Fungsi agar menu mobile tertutup saat item di-klik
+menu_items.forEach((item) => {
+	item.addEventListener('click', () => {
+		// Hanya tutup jika menu sedang terbuka
+		if (mobile_menu.classList.contains('active')) {
+			hamburger.classList.remove('active');
+			mobile_menu.classList.remove('active');
+		}
+	});
 });
