cd3d80cc461ac128da9fa6cfbb9ed04f1f9ac5a2
fajarraden09 <fajarraden9019@gmail.com>
2025-09-27 18:19:01 +0700

    tenence 8

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
 
+// Hanya untuk membuka & menutup menu hamburger di mobile
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
-
+// Agar menu mobile tertutup saat item di-klik
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
